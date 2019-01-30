73656ca01b007114830e31926b1a8af868513079
hibozzy18 <hknguruwe@gmail.com>
2019-01-30 22:39:05 +0100

    add rationale functions

--- tabulation.py
+++ tabulation.py
@@ -14,13 +14,16 @@ if(len(sys.argv) == 2):
     option = sys.argv[1]
     if(int(option) == 1):
     	yval = [x for x in xval]
-    	pass
+    elif(int(option) == 2):      
+    	yval = [np.exp(x) for x in xval]
+    elif(int(option) == 3):
+    	yval = [np.sqrt(abs(x)) for x in xval]
 else:
 	print(""""
     Wrong argument list!!. 
     Usage:Tabulation.py N where N is a number between from 1 to 8 inclusive.\n
     N has the following options:
-    1 ---   f(x) 
+    1 ---   f(x) \t 2 ---  exp(x) \t 3 --- sqrt(abs(x))
     """)
     sys.exit(1)
 
