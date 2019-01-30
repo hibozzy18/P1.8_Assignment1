b52147fbe476d38506c4c5dd98f18641b1438b10
hibozzy18 <hknguruwe@gmail.com>
2019-01-30 21:57:55 +0100

    add funct 2 & 3

--- tabulation.py
+++ tabulation.py
@@ -13,8 +13,12 @@ xval = [x for x in pl.frange(-5.0, 5.0, 0.1)]
 if(len(sys.argv) == 2):
     option = sys.argv[1]
     if(int(option) == 1):
-    	yval = [x for x in xval]
-    	pass
+    	yval = [x for x in xval]    
+    elif(int(option) == 2):
+    	yval = [x**2 for x in xval]
+    elif(int(option) == 3):
+        yval = [x**3 for x in xval]
+    
 else:
 	pass
 
