a4bd91d972721b47fb0fc4bc8ebaf7fe3ec3f4fe
hibozzy18 <hknguruwe@gmail.com>
2019-01-30 21:46:45 +0100

    fill lists

--- tabulation.py
+++ tabulation.py
@@ -9,12 +9,12 @@ from matplotlib import pyplot as plt
 from matplotlib import pylab as pl
 import numpy as np
 
-
+xval = [x for x in pl.frange(-5.0, 5.0, 0.1)]
 if(len(sys.argv) == 2):
     option = sys.argv[1]
     if(int(option) == 1):
+    	yval = [x for x in xval]
     	pass
-
 else:
 	pass
-	
+
