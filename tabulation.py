a33a12cacfe6446e6a58d486f69e713b4549137e
hibozzy18 <hknguruwe@gmail.com>
2019-01-30 21:45:34 +0100

    command line

---
+++ tabulation.py
@@ -0,0 +1,20 @@
+""" The script is used to plot variables of x against  variables 
+of y """
+from __future__ import print_function
+import sys
+
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib import pyplot as plt
+from matplotlib import pylab as pl
+import numpy as np
+
+
+if(len(sys.argv) == 2):
+    option = sys.argv[1]
+    if(int(option) == 1):
+    	pass
+
+else:
+	pass
+	
